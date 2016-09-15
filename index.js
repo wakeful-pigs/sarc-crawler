bf681dac1720649b3a619d19cf9d6b240a4e872a
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-09-15 02:33:49 +0000

    Starting with express

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const crawler =  require('./crawler');
+const express = require('express');
+const PORT = 3000;
 
-let crawler_handler = (err, data) => {
-	if(err) throw err;
-	console.log(data);
-};
+//let crawler_handler = (err, data) => {
+	//if(err) throw err;
+	//console.log(data);
+//};
 
-crawler(crawler_handler);
+//crawler(crawler_handler);
+
+let root_handler = (req, res) =>{
+	crawler((err, data) => {
+		if(err) throw err;
+		res.send(data);
+	})
+} 
+let listen_handler = () => console.log(`Server running at: ${PORT}`);
+
+var app = express();
+app.get('/', root_handler);
+app.listen(PORT, listen_handler)
